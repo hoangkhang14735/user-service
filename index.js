3ede4f6514248f8489377de236fcd6bafb1a4df5
“TrinhNguyenHoangHuy” <lecaoanhtri147@gmail.com>
2026-05-13 23:11:05 +0700

    update-1

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ app.post('/register', async (req, res) => {
     // TODO: Hash the password using bcrypt. `bcrypt.hash(password, 10)`
     // TODO: Create a new User instance with the username and hashed password.
     // TODO: Save the new user to the database.
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
     res.status(201).json({ message: 'User created', token: signToken(user) });
   } catch (err) {
     console.error('Register error:', err);
